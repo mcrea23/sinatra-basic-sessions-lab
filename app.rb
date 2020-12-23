0896df3b35bfd2525bcf12bffb2edc931ef4b04b
+ <mcrea23@aol.com>
2020-12-23 11:36:51 -0600

    commit tree 8e6b8a5b9ae8ad9f3a44848a149a21b9cf103d27

--- app.rb
+++ app.rb
@@ -1,4 +1,20 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+  configure do
+    enable :sessions
+    set :session_secret, "merp"
+  end 
+
+get '/' do
+  erb :index
+end
+
+post '/checkout' do
+  session[:item] = params[:item]
+  @sessions = session
+
+  erb :checkout
+end
 end
